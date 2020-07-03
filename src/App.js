cb77ad9c1cb6e7f6bcb386c576d4f3ea487b5504
Rainspistols <ffly@mail.ru>
2020-07-03 18:08:42 +0200

    Post Card Details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 // Components
 import Navbar from './components/Navbar';
 
@@ -10,20 +11,43 @@ import home from './pages/home.js';
 import login from './pages/login.js';
 import signup from './pages/signup.js';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff',
+    },
+    // ? Delete or use
+    // typography: {
+    //   useNextVariants: true,
+    // },
+  },
+});
+
 function App() {
   return (
-    <div className='App'>
-      <Router>
-        <Navbar /> 
-        <div className='container'>
-          <Switch>
-            <Route path='/' exact component={home} />
-            <Route path='/login' exact component={login} />
-            <Route path='/signup' exact component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className='App'>
+        <Router>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route path='/' exact component={home} />
+              <Route path='/login' exact component={login} />
+              <Route path='/signup' exact component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
