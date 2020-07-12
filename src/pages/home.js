ef0399f71635d982a095cec5019bb8b766d18bc5
Rainspistols <ffly@mail.ru>
2020-07-12 19:14:15 +0200

    Logout and Edit Profile finish

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,31 +1,21 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
-
+import PropTypes from 'prop-types';
+// Components
 import Scream from '../components/Scream';
-import Profile from '../components/Profile'
+import Profile from '../components/Profile';
+// Redux Stuff
+import { connect } from 'react-redux';
+import { getScreams } from '../redux/actions/dataActions';
 
 class home extends Component {
-  state = {
-    screams: null,
-  };
   componentDidMount() {
-    axios
-      .get('/screams')
-      .then((res) => {
-        console.log(res.data);
-        this.setState({
-          screams: res.data,
-        });
-      })
-      .catch((err) => console.log(err));
+    this.props.getScreams();
   }
-
   render() {
-    let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map((scream) => (
-        <Scream scream={scream} key={scream.screamId} />
-      ))
+    const { screams, loading } = this.props.data;
+    let recentScreamsMarkup = !loading ? (
+      screams.map((scream) => <Scream scream={scream} key={scream.screamId} />)
     ) : (
       <p>Loading ...</p>
     );
@@ -42,4 +32,13 @@ class home extends Component {
   }
 }
 
-export default home;
+home.propTypes = {
+  getScreams: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  data: state.data,
+});
+
+export default connect(mapStateToProps, { getScreams })(home);
